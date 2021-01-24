3d02c25fd7244f18088db889d4f4fe0349fe498e
Aqudi <gjdigj145@gmail.com>
2021-01-24 21:01:53 +0900

    feat: Update tflite_service
    classifyImageFromCamera
    + numResults 2로 qusrud
    + verbose 로그 이동
    
    stopClassifyImageFromCamera
    + 메소드 생성
    
    _updateResult
    + 필요할 때만 업데이트 되도록
    + Stream이 닫혔을 때 다시 열도록

--- lib/services/tflite_service.dart
+++ lib/services/tflite_service.dart
@@ -8,10 +8,8 @@ import '../models/tflite_result.dart';
 import 'logger_service.dart';
 
 class TfLiteService {
-  StreamController<List<TfLiteResult>> streamController =
+  StreamController<List<TfLiteResult>> _streamController =
       StreamController.broadcast();
-
-  final _outputs = <TfLiteResult>[];
 
   bool isBusy = false;
   bool isModelLoaded = false;
@@ -24,10 +22,11 @@ class TfLiteService {
 
   TfLiteService._internal();
 
-  Stream<List<TfLiteResult>> get stream => streamController.stream;
+  Stream<List<TfLiteResult>> get stream => _streamController.stream;
 
   Future<String> loadModel() async {
     logger.verbose("loadModel");
+    _streamController.add(null);
     return Tflite.loadModel(
       model: "assets/model.tflite",
       labels: "assets/labels.txt",
@@ -41,9 +40,10 @@ class TfLiteService {
     });
   }
 
-  void classifyImageFile(File image) async {
+  Future<void> classifyImageFile(File image) async {
     if (isModelLoaded && !isBusy) {
       logger.verbose("classifyImageFile");
+      isBusy = true;
       await Tflite.runModelOnImage(
         path: image.path,
         numResults: 2,
@@ -51,55 +51,66 @@ class TfLiteService {
         if (results.isNotEmpty) {
           _updateResult(results);
         }
-        isBusy = false;
       }).catchError((e) {
         isModelLoaded = false;
-        isBusy = false;
         logger.error(e.toString());
       });
+      isBusy = false;
     }
   }
 
   void classifyImageFromCamera(CameraImage image) async {
-    logger.verbose("classifyImageFromCamera");
-    logger.debug(isModelLoaded && !isBusy);
-
     if (isModelLoaded && !isBusy) {
+      logger.verbose("classifyImageFromCamera");
       isBusy = true;
       await Tflite.runModelOnFrame(
         bytesList: image.planes.map((plane) {
           return plane.bytes;
         }).toList(),
-        numResults: 5,
+        numResults: 2,
       ).then((results) {
         if (results.isNotEmpty) {
           _updateResult(results);
         }
-        isBusy = false;
       }).catchError((e) {
         isModelLoaded = false;
-        isBusy = false;
         logger.error(e.toString());
       });
+      isBusy = false;
+    }
+  }
+
+  void stopClassifyImageFromCamera() async {
+    if (!_streamController.isClosed) {
+      _streamController.add(null);
+      _streamController.close();
     }
   }
 
   void _updateResult(results) {
-    _outputs.clear();
-    for (final result in results) {
-      _outputs.add(
-        TfLiteResult(
-          id: result['index'],
-          confidence: result['confidence'],
-          label: result['label'],
-        ),
-      );
-      var msg = "index:  ${result['index']}\n";
-      msg += "confidence: ${result['confidence']}\n";
-      msg += "label: ${result['label']}";
-      logger.verbose(msg);
+    if (results.isNotEmpty) {
+      final _outputs = <TfLiteResult>[];
+      for (final result in results) {
+        _outputs.add(
+          TfLiteResult(
+            id: result['index'],
+            confidence: result['confidence'],
+            label: result['label'],
+          ),
+        );
+        var msg = "index:  ${result['index']}\n";
+        msg += "confidence: ${result['confidence']}\n";
+        msg += "label: ${result['label']}";
+        logger.verbose(msg);
+      }
+      _outputs.sort((a, b) => a.confidence.compareTo(b.confidence));
+
+      if (_streamController.isClosed) {
+        logger.verbose("Init streamcontroller");
+        _streamController = StreamController.broadcast();
+        _streamController.add(null);
+      }
+      _streamController.add(_outputs);
     }
-    _outputs.sort((a, b) => a.confidence.compareTo(b.confidence));
-    streamController.add(_outputs);
   }
 }
